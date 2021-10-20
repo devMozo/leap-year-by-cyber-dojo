ecad02e30c3f3677fa56e2fd51f95e92a29b02df
Mozo <devnicolasmozo@gmail.com>
2021-10-20 14:17:29 +0200

    Test done

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,3 +1,21 @@
-describe("", () => {
+const LeapYearAnalyzer = require("../index.js");
+
+describe("isALeapYear should", () => {
+
+    test("retrieve true if the year is leap", () => {
+        expect(LeapYearAnalyzer.isALeapYear(1996)).toBeTruthy();
+    });
+
+    test("retrieve false if the year is not leap", () => {
+        expect(LeapYearAnalyzer.isALeapYear(2001)).toBeFalsy();
+    });
+
+    test("retrieve true if the year is divisible by 4", () => {
+       expect(LeapYearAnalyzer.isALeapYear(2000)).toBeTruthy();
+    });
+
+    test("retrieve false if the year is divisible by 100", () => {
+        expect(LeapYearAnalyzer.isALeapYear(1900)).toBeFalsy();
+    });
 
 });
---
+++ index.js
@@ -0,0 +1,9 @@
+const isALeapYear = (year) => {
+    const isDivisibleByFour = year % 4 === 0;
+    const isDivisibleByOneHundred = year % 100 === 0;
+    const isDivisibleByFourHundred = year % 400 === 0;
+
+    return isDivisibleByFour && !(isDivisibleByOneHundred && !isDivisibleByFourHundred);
+}
+
+exports.isALeapYear = isALeapYear;
